dc1626badf9892e5d0b176dad576972b9787a5b5
jm47895 <jordan.madrigal@gmail.com>
2020-04-23 14:19:08 -0500

    Add base code for testing sql database access

---
+++ app/src/androidTest/java/com/jordanmadrigal/notesappplus/NoteDatabaseTest.java
@@ -0,0 +1,35 @@
+package com.jordanmadrigal.notesappplus;
+
+import android.app.Application;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.jordanmadrigal.notesappplus.models.Note;
+import com.jordanmadrigal.notesappplus.persistence.NoteDao;
+import com.jordanmadrigal.notesappplus.persistence.NoteDatabase;
+import com.jordanmadrigal.notesappplus.util.TestUtil;
+
+import org.junit.After;
+import org.junit.Before;
+
+public abstract class NoteDatabaseTest {
+
+    private NoteDatabase noteDatabase;
+    Note note = TestUtil.NOTE;
+
+    public NoteDao getNoteDao(){
+        return noteDatabase.getNoteDao();
+    }
+
+    @Before
+    public void init(){
+        noteDatabase = Room.inMemoryDatabaseBuilder(
+                ApplicationProvider.getApplicationContext(), NoteDatabase.class).build();
+    }
+
+    @After
+    public void finish(){
+        noteDatabase.close();
+    }
+}
---
+++ app/src/test-common/java/com/jordanmadrigal/notesappplus/util/TestUtil.java
@@ -0,0 +1,8 @@
+package com.jordanmadrigal.notesappplus.util;
+
+import com.jordanmadrigal.notesappplus.models.Note;
+
+public class TestUtil {
+
+    public static Note NOTE = new Note();
+}
