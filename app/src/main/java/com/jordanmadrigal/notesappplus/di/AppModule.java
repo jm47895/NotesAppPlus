7e9c176cb8656a577c3886ae57e9a1bf6a307042
jm47895 <jordan.madrigal@gmail.com>
2020-04-23 11:37:45 -0500

    Set up room and put in appmodule

--- app/src/main/java/com/jordanmadrigal/notesappplus/di/AppModule.java
+++ app/src/main/java/com/jordanmadrigal/notesappplus/di/AppModule.java
@@ -1,10 +1,32 @@
 package com.jordanmadrigal.notesappplus.di;
 
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.jordanmadrigal.notesappplus.persistence.NoteDao;
+import com.jordanmadrigal.notesappplus.persistence.NoteDatabase;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+
+import static com.jordanmadrigal.notesappplus.persistence.NoteDatabase.DATABASE_NAME;
 
 @Module
 class AppModule {
 
+    @Singleton
+    @Provides
+    static NoteDatabase provideNoteDatabase(Application application){
+        return Room.databaseBuilder(application, NoteDatabase.class, DATABASE_NAME).build();
+    }
 
+    @Singleton
+    @Provides
+    static NoteDao provideNoteDao(NoteDatabase noteDatabase){
+        return noteDatabase.getNoteDao();
+    }
 
 }
